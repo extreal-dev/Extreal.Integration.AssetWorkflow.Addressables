0a3b6a20c5137b4631fa64ad56b830682ee73d15
sus-taguchi-t <taguchi-t@m.sus-g.co.jp>
2023-03-13 14:15:46 +0900

    Remove tests

--- Tests/Runtime/ResourceProviders/AesCbcAssetBundleProvider.cs
+++
@@ -1,8 +0,0 @@
-namespace Extreal.Integration.Assets.Addressables.ResourceProviders.Test
-{
-    [System.ComponentModel.DisplayName("AES-CBC AssetBundle Provider")]
-    public class AesCbcAssetBundleProvider : CryptoAssetBundleProviderBase
-    {
-        public override ICryptoStreamFactory CryptoStreamFactory => new AesCbcStreamFactory();
-    }
-}
--- Tests/Runtime/ResourceProviders/FailedCryptAssetBundleProvider.cs
+++
@@ -1,8 +0,0 @@
-namespace Extreal.Integration.Assets.Addressables.ResourceProviders.Test
-{
-    [System.ComponentModel.DisplayName("Failed Crypto AssetBundle Provider")]
-    public class FailedCryptoAssetBundleProvider : CryptoAssetBundleProviderBase
-    {
-        public override ICryptoStreamFactory CryptoStreamFactory => new FailedCryptoStreamFactory();
-    }
-}
